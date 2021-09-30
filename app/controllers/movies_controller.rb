be02ed73d9f42e8d9f38c72a2a77d4313005a7a6
EC2 Default User <ec2-user@ip-172-31-52-202.ec2.internal>
2021-09-30 00:07:13 +0000

    final push

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,18 +12,47 @@ class MoviesController < ApplicationController
       @query = Movie
       @movies = @query.all
       @allRatings = Movie.uniq.pluck('rating')
-      if params.has_key?('sort_by') and Movie.column_names.include?(params[:sort_by])
-        @sortedBy = params[:sort_by]
-        @movies = @query.all.order("#{@sortedBy}")
+      
+      if !params.has_key?('ratings_filter') and !params.has_key?('sort_by') and (session.has_key?('ratings_filter') or session.has_key?('sort_by'))
+        puts "[X] Redirection + Populating from session - #{session[:ratings_filter]} #{session[:sort_by]}"
+        if session.has_key?('ratings_filter')
+          params[:ratings_filter] = session[:ratings_filter]
+        end
+        if session.has_key?('sort_by')
+          params[:sort_by] = session[:sort_by]
+        end
+        redirect_to movies_path(params)
       end
+      
       if params.has_key?('ratings_filter')
         puts "filter tags #{@filterRatings} #{params[:ratings_filter]}"
         @filterRatings = params[:ratings_filter].keys
-        @movies = @query.where(rating: @filterRatings).all
+        @query = @query.where(rating: @filterRatings)
+        session[:ratings_filter] = params[:ratings_filter]
+        puts "[X] Collecting ratings_filter from request - #{@filterRatings}"
+      elsif session.has_key?('ratings_filter')
+        @filterRatings = session[:ratings_filter].keys
+        @query = @query.where(rating: @filterRatings)
+        puts "[X] Collecting ratings_filter from session - #{@filterRatings}"
       else
-        puts "filter tags refresh #{@filterRatings}"
         @filterRatings = @allRatings
+        @query = @query.where(rating: @filterRatings)
+        puts "[X] Collecting ratings_filter from default - #{@filterRatings}"
       end
+      
+      if (params.has_key?('sort_by') and Movie.column_names.include?(params[:sort_by]))
+        @sortedBy = params[:sort_by] 
+        @movies = @query.all.order("#{@sortedBy}")
+        session[:sort_by] = @sortedBy
+        puts "[X] Explicit sort_by from request - #{@sortedBy}"
+      elsif session.has_key?('sort_by')
+        @sortedBy = session[:sort_by] 
+        @movies = @query.all.order("#{@sortedBy}")
+        puts "[X] Collecting sort_by from session - #{@sortedBy}"
+      else
+        @movies = @query.all
+      end
+    
     end
   
     def new
